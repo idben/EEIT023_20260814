195c01cec1ded5010899a8054b312e7b77fa9f99
Ben Chen <idben.chen@gmail.com>
2026-08-14 11:35:17 +0800

    操作05 使用者資料的持久化

--- pinia01/src/stores/user.js
+++ pinia01/src/stores/user.js
@@ -4,17 +4,20 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore(
     'user',
     () => {
-        const isLogin = ref(false)
-        const user = ref(null)
+        const savedUser = localStorage.getItem('user0001')
+        const user = ref(savedUser ? JSON.parse(savedUser) : null)
+        const isLogin = ref(user.value ? true : false)
 
         function login(userData) {
             user.value = userData
             isLogin.value = true
+            localStorage.setItem('user0001', JSON.stringify(userData))
         }
 
         function logout() {
             user.value = null
             isLogin.value = false
+            localStorage.removeItem('user0001')
         }
 
         return { isLogin, user, login, logout }
