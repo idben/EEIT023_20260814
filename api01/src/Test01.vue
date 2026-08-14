c1d4858764c65102f9d81ae5c51a654adab732ae
Ben Chen <idben.chen@gmail.com>
2026-08-14 12:00:39 +0800

    操作1 第一次呼叫API

---
+++ api01/src/Test01.vue
@@ -0,0 +1,28 @@
+<script setup>
+// 匯入 Vue 功能與 Axios
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+// 宣告響應式狀態
+const products = ref([])
+
+// 定義取得商品的函式
+const fetchProducts = async () => {
+    const response = await axios.get('https://fakestoreapi.com/products')
+    products.value = await response.data
+    console.log(products.value);
+
+}
+
+// 元件掛載後呼叫 API
+onMounted(() => {
+    fetchProducts()
+})
+</script>
+
+<template>
+    <h1>呼叫 API</h1>
+    <!-- 標題與商品列表 -->
+</template>
+
+<style scoped></style>
